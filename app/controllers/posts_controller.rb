5af404bef44d257a48574562f0ba2e84cba0edbb
sekitaku <sekitaku56@gmail.com>
2019-06-05 13:46:33 +0900

    投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    if params[:image]
+      @post = Post.new()
+      @post.image_name = "#{@post.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/food_images/#{@post.image_name}", image.read)
+      @post.save
+      redirect_to("/posts/index")
+    else
+      flash[:notice] = "画像のアップロードに失敗しました……"
+      render("posts/new")
+    end
+  end
+
+  def create_to_new
+    redirect_to("/posts/new")
   end
 end
